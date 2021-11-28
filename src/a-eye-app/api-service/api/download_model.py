5d3dc668ee566a2b7ee2a56408904ef0ba66b8f6
Anita Mahinpei <anita.mahinpei@gmail.com>
2021-11-28 02:05:37 -0600

    edit model download

--- src/a-eye-app/api-service/api/download_model.py
+++ src/a-eye-app/api-service/api/download_model.py
@@ -2,7 +2,7 @@ import os
 gcp_project = os.environ["GCP_PROJECT"]
 bucket_name = "a-eye-app-model"
 local_models_path = "/persistent/model_weights/"
-source_blob_name = "inception_gru_full_epoch=5/"
+source_blob_names = ["flickr8_VGG_LSTM" "inception_gru_full_epoch=5/"]
 # note: run `pipenv install gsutil` if gsutil is not installed
 # run `gsutil config` to get access 
 def download_bucket_objects(bucket_name, blob_path, local_path):
@@ -10,5 +10,6 @@ def download_bucket_objects(bucket_name, blob_path, local_path):
     command = "gsutil cp -r gs://{bucketname}/{blobpath} {localpath}".format(bucketname = bucket_name, blobpath = blob_path, localpath = local_path)
     os.system(command)
     return command
-download_bucket_objects(bucket_name, source_blob_name, local_models_path)
+for source_blob_name in source_blob_names:
+    download_bucket_objects(bucket_name, source_blob_name, local_models_path)
 print("Done!")
